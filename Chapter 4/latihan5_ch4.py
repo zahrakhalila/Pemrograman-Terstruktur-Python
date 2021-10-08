1ffd7aea7a909ccca075551770320a80d27f190b
zahrakhalila <zkhalilas1524@student.uns.ac.id>
2021-10-08 18:16:27 +0700

    Add latihan5_ch4

---
+++ Chapter 4/latihan5_ch4.py
@@ -0,0 +1,9 @@
+#Jumlah mahasiswa PTIK
+jumlahlk=int(input("Jumlah mahasiswa laki-laki PTIK 2021:"))
+jumlahpr=int(input("Jumlah mahasiswa perempuan PTIK 2021:"))
+print("__________________________________________________________")
+
+#Diagram batang
+print("Diagram batang horizontal jumlah mahasiswa PTIK 2021")
+print("Laki-Laki:","*"*jumlahlk)
+print("Perempuan:","*"*jumlahpr)
