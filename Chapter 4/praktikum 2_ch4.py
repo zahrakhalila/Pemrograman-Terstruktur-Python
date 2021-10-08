a39591287c96aa1aa0e525495718ebd28f05c00f
zahrakhalila <zkhalilas1524@student.uns.ac.id>
2021-10-08 18:16:59 +0700

    Add praktikum2_ch4

---
+++ Chapter 4/praktikum 2_ch4.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x+y))
+
+
+# In[2]:
+
+
+a = 2
+b = 2.53
+print(type(a+a))
+print(type(a+b))
+print(type(b+a))
+print(type(b+b))
+
+
+# In[3]:
+
+
+a = 2
+b = 2.53
+print(type(a-a))
+print(type(a-b))
+print(type(b-a))
+print(type(b-b))
+
+
+# In[4]:
+
+
+a = 2
+b = 2.53
+print(type(a*a))
+print(type(a*b))
+print(type(b*a))
+print(type(b*b))
+
+
+# In[5]:
+
+
+a = 2
+b = 2.53
+print(type(a/a))
+print(type(a/b))
+print(type(b/a))
+print(type(b/b))
+
+
+# In[6]:
+
+
+a = 2
+b = 2.53
+print(type(a//a))
+print(type(a//b))
+print(type(b//a))
+print(type(b//b))
+
+
+# In[7]:
+
+
+a = 2
+b = 2.53
+print(type(a%a))
+print(type(a%b))
+print(type(b%a))
+print(type(b%b))
+
+
+# In[8]:
+
+
+a = 2
+b = 2.53
+print(type(a**a))
+print(type(a**b))
+print(type(b**a))
+print(type(b**b))
+
+
+# In[9]:
+
+
+a = 10
+p = y = x = z = a
+print(a)
+print(z)
+print(x)
+print(y)
+print(p)
+
+
+# In[ ]:
+
+
+
+
