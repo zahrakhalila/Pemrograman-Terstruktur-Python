43c6922f2ca5ca90deec9582bb300fcd84c195f0
zahrakhalila <zkhalilas1524@student.uns.ac.id>
2021-10-08 18:16:43 +0700

    Add praktikum1_ch4

---
+++ Chapter 4/praktikum 1_ch4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+type(10)
+
+
+# In[2]:
+
+
+type(10.1918)
+
+
+# In[3]:
+
+
+type('Hello World')
+
+
+# In[4]:
+
+
+type(True)
+
+
+# In[ ]:
+
+
+
+
